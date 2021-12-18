13ab0238cdd572c55fee8393592973dcb7a0765b
HyungwonJin <86827200+HyungwonJin@users.noreply.github.com>
2021-12-18 17:58:18 +0900

    feat: Header Setting

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,26 @@
-import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Header from "./Components/Header";
+import Home from "./Routes/Home";
+import Search from "./Routes/Search";
+import Tv from "./Routes/Tv";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Router>
+      <Header />
+      <Switch>
+        <Route path="/">
+          <Home />
+        </Route>
+        <Route path="/tv">
+          <Tv />
+        </Route>
+        <Route path="/search">
+          <Search />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
